9cb7cd4b63f9985452a563ce91a75756ba893b78
emma <sonicjane2020@gmail.com>
2020-03-13 08:38:26 -0700

    Made class components in app.js. Wrote an axios request, got data correctly.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Lambda School Advanced React Sprint</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      players: []
+    };
+    console.log('constructor runs');
+  }
+
+  componentDidMount() {
+    console.log("cDM mounted");
+    axios.get(`http://localhost:5000/api/players`)
+      .then(results => {
+        const players = results.data;
+        this.setState({... players})
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+      <h1>Lambda School Advanced React Sprint</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
