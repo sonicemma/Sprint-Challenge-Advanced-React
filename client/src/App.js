6f39c880c1479e4e6d5bb5cfe224271010028b4d
emma <sonicjane2020@gmail.com>
2020-03-13 08:52:00 -0700

    Data displayed correctly in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -16,14 +16,23 @@ class App extends React.Component {
     console.log("cDM mounted");
     axios.get(`http://localhost:5000/api/players`)
       .then(results => {
-        const players = results.data;
-        this.setState({... players})
+        this.setState({
+          players: results.data
+        })
       });
   }
   render() {
     return (
       <div className="App">
       <h1>Lambda School Advanced React Sprint</h1>
+      <div className="PlayerCard">
+        {this.state.players.map(player => 
+          <h2>Name: {player.name}<br/>
+          Country: {player.country}<br/>
+          Searches: {player.searches}
+          </h2>
+          )}
+      </div>
       </div>
     );
   }
